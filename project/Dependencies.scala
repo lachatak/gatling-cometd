8d80b8216e599481a36e95eb072612c01fb46349
Krisztian Lachata <krisztian.lachata@gmail.com>
2014-10-15 23:03:52 +0100

    use latest cometd release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Version {
 
-  val gatling       = "2.0.0-RC6"
-  val gatlingTest   = "1.0-RC6"
+  val gatling       = "2.0.1"
+  val gatlingTest   = "1.0"
   val akka          = "2.3.4"
   val jackson       = "2.4.1"
   val scalaTest     = "2.2.0"
