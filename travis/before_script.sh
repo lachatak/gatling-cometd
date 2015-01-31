435f0cbf5d0c2ce2a8d8b144bf93719efb21d434
Krisztian Lachata <krisztian.lachata@gmail.com>
2015-01-31 21:30:05 +0000

    Update before_script.sh

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 travis/make_credentials.sh
 travis/start_server.sh
-
-java -version
-docker -version
-uname -a
-sudo apt-get install apt-transport-https
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
-sudo apt-get install lxc-docker -y
